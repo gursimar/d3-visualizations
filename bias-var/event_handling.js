3a5f3e4cdf761ec754ac8f51475f55d20583dc7e
Gursimar <simar.i3r@gmail.com>
2017-12-11 13:08:52 -0800

    dataset images and buttoms added; idea taken from tensorflow

--- bias-var/event_handling.js
+++ bias-var/event_handling.js
@@ -1,5 +1,10 @@
 // EVENT HANDLING
-$("#data_1").click(function() {
+//$("#data_1").click(function() {
+$("#DS_c").click(function() {
+	//this.className+=' selected'
+	$( "#DS_c" ).addClass("selected" );
+	$( "#DS_b" ).removeClass("selected" );
+	$( "#DS_a" ).removeClass("selected" );
 	w_ind = 0
 	data = createData()
 	clearBoxes()
@@ -7,7 +12,11 @@ $("#data_1").click(function() {
 	drawData(data);
 });
 
-$("#data_2").click(function() {
+//$("#data_2").click(function() {
+$("#DS_a").click(function() {
+	$( "#DS_b" ).removeClass("selected" );
+	$( "#DS_a" ).addClass("selected" );
+	$( "#DS_c" ).removeClass("selected" );
 	w_ind = 1
 	data = createData()
 	clearBoxes()
@@ -16,7 +25,11 @@ $("#data_2").click(function() {
 });
 
 
-$("#data_3").click(function() {
+//$("#data_3").click(function() {
+$("#DS_b").click(function() {
+	$( "#DS_a" ).removeClass("selected" );
+	$( "#DS_c" ).removeClass("selected" );
+	$( "#DS_b" ).addClass("selected" );
 	w_ind = 2
 	data = createData()
 	clearBoxes()
